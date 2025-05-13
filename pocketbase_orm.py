b375b766eab84f04c41257eb98c5f58e4477f181
Jannis Schönleber <joennlae@gmail.com>
2025-05-13 16:02:25 +0200

    env: bump version

--- pocketbase_orm.py
+++ pocketbase_orm.py
@@ -8,7 +8,7 @@ from pocketbase import PocketBase
 from pocketbase.client import FileUpload
 from pydantic import AnyUrl, BaseModel, EmailStr, Field
 
-__version__ = "0.17.1"
+__version__ = "0.17.2"
 
 logger = logging.getLogger(__name__)
 
