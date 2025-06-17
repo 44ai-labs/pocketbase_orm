89103a6c5f3fc8a01182c6c83d5a3a0c0f44caad
Andrin Benz <andrin.benz@gmx.ch>
2025-06-17 09:46:06 +0000

    error safe fallback

--- pocketbase_orm.py
+++ pocketbase_orm.py
@@ -324,7 +324,7 @@ class PBModel(BaseModel):
             field_dict = {
                 "name": field["name"],
                 "type": field["type"],
-                "id": field["id"],
+                "id": field["id"] if "id" in field else None,
                 "required": field["required"] if "required" in field else False,
                 "system": field["system"],
                 "onCreate": field["onCreate"] if "onCreate" in field else False,
